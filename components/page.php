e33a6af553c1ccd23c3f260a841cd874a6208cbd
Marc Anton Dahmen <mail@marcdahmen.de>
2024-12-09 20:20:31 +0100

    remove meta tags from theme

--- components/page.php
+++ components/page.php
@@ -8,15 +8,7 @@ https://marcdahmen.de
 #><!DOCTYPE html>
 <html lang="en" class="@{ template | sanitize }<@ if @{ selectColorTheme | def ('switcher') } != 'switcher' @> @{ selectColorTheme }<@ end @>">
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>@{ metaTitle | def ('@{title} | @{ sitename }')}</title>
-	<@ with @{ imageFavicon } ~@>
-		<link href="@{ :file }" rel="shortcut icon" type="image/x-icon" />
-	<@ end ~@>
-	<@ with @{ imageAppleTouchIcon } ~@>
-		<link href="@{ :file }" rel="apple-touch-icon" />
-	<@~ end @>
 	<link href="/packages/@{ theme }/dist/vendor.bundle.css" rel="stylesheet">
 	<link href="/packages/@{ theme }/dist/main.bundle.css" rel="stylesheet">
 	<script src="/packages/@{ theme }/dist/vendor.bundle.js" type="text/javascript"></script>
